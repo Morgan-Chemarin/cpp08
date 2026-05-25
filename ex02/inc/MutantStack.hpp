3530c613d96fb0341c2b0459824f3086d8b6b795
Morgan Chemarin <mchemari@z2r2p2.42lyon.fr>
2026-05-25 14:04:02 +0200

    FIX : ex02 refacto useless char

--- ex02/inc/MutantStack.hpp
+++ ex02/inc/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: mchemari <mchemari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 11:38:02 by mchemari          #+#    #+#             */
-/*   Updated: 2026/05/25 12:19:27 by mchemari         ###   ########.fr       */
+/*   Updated: 2026/05/25 13:59:31 by mchemari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,20 @@ class MutantStack: public std::stack<T>
 		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-		iterator begin() {return this->c.begin();};
-		iterator end() {return this->c.end();};
+		iterator begin() {return this->c.begin();}
+		iterator end() {return this->c.end();}
 
-		const_iterator begin() const {return this->c.begin();};
-		const_iterator end() const {return this->c.end();};
+		const_iterator begin() const {return this->c.begin();}
+		const_iterator end() const {return this->c.end();}
 
-		reverse_iterator rbegin(void) {return this->c.rbegin();};
-		reverse_iterator rend(void) {return this->c.rend();};
+		reverse_iterator rbegin() {return this->c.rbegin();}
+		reverse_iterator rend() {return this->c.rend();}
 
-		const_reverse_iterator rbegin(void) const {return this->c.rbegin();};
-		const_reverse_iterator rend(void) const {return this->c.rend();};
+		const_reverse_iterator rbegin() const {return this->c.rbegin();}
+		const_reverse_iterator rend() const {return this->c.rend();}
 
 		MutantStack(): std::stack<T>() {};
-		MutantStack(const MutantStack<T>& other): std::stack<T>(other) {};
+		MutantStack(const MutantStack<T>& other): std::stack<T>(other) {}
 		MutantStack<T>& operator=(const MutantStack<T>& other)
 		{
 			if (this != &other)
